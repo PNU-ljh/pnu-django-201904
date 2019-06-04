0dce1d29e5ad8168ccb1da398a874b21d7ed9ff7
PNU-ljh <ljh@pusan.ac.kr>
2019-06-04 17:26:54 +0900

    190604

---
+++ travel/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'travel'
+
+urlpatterns = [
+    # travel/ => Post List
+    path('', views.post_list, name='post_list'),
+    # travel/숫자/ => Post Detail
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:post_pk>/comments/new/', views.comment_new, name='comment_new'),
+    path('<int:post_pk>/comments/<int:pk>/edit/', views.comment_edit, name='comment_edit'),
+    path('<int:post_pk>/comments/<int:pk>/delete/', views.comment_delete, name='comment_delete'),
+]
---
+++ travel/views.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Post, Comment
+from .forms import CommentForm
+
+
+def post_list(request):
+    return render(request, 'travel/post_list.html', {
+        'post_list': Post.objects.all(),
+    })
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    # comment_list = post.comment_set.all()
+    # # comment_list = Comment.objects.filter(post=post)
+    # comment_list = comment_list.order_by('-id')
+
+    return render(request, 'travel/post_detail.html', {
+        'post': post,
+        # 'comment_list': comment_list,
+        'comment_form': CommentForm(),
+    })
+
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('travel:post_detail', post_pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'travel/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_edit(request, post_pk, pk):
+    comment = get_object_or_404(Comment, author=request.user, pk=pk)
+    # if comment.author != request.user:
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('travel:post_detail', post_pk)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, 'travel/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_delete(request, post_pk, pk):
+    comment = get_object_or_404(Comment, author=request.user, pk=pk)
+    if request.method == 'POST':
+        comment.delete()  # 호출 즉시 DELETE 쿼리 수행
+        return redirect('travel:post_detail', post_pk)
+    return render(request, 'travel/post_confirm_delete.html', {
+        'comment': comment,
+    })
